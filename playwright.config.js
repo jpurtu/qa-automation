8ceff70f16b5e6d613fda77470f647c8d1d5d4fd
jpurtu <TU_EMAIL@gmail.com>
2026-08-23 15:25:06 -0400

    feat: agregar Allure Report

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,10 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   testIgnore: '**/performance/**',
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   projects: [
     {
       name: 'chromium',
