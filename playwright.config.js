3b4cff5d38226c63b7aa8a66becacf5f94b43c54
jpurtu <TU_EMAIL@gmail.com>
2026-08-23 13:55:17 -0400

    feat: agregar testing multi-navegador Chrome y Firefox

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,16 @@
-const { defineConfig } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
   testIgnore: '**/performance/**',
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
 });
